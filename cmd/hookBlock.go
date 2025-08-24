9ac12c1a40d514b0b2559df6475dbe000cd1f10e
Walid Baroudi <waleedbaroudi@hotmail.com>
2025-08-24 22:28:26 +0300

    Fix Hook Path

--- cmd/hookBlock.go
+++ cmd/hookBlock.go
@@ -16,6 +16,8 @@ func init() {
 	hookStart = "# nocommit hook start"
 	hookEnd = "# nocommit hook end"
 	home, _ := os.UserHomeDir()
-	HookLine = filepath.Join(home, ".nocommit", "hooks", "pre-commit")
+	hookPath := filepath.Join(home, ".nocommit", "hooks", "pre-commit")
+	correctedHookPath := "\"" + filepath.ToSlash(hookPath) + "\""
+	HookLine = "sh " + correctedHookPath
 	hookBlock = hookStart + "\n" + HookLine + "\n" + hookEnd + "\n"
 }
